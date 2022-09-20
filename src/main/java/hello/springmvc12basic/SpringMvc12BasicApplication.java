8fc8eca196d67b7bd07bc9088d3b311f7f5151ce
xhaktmchl <xhaktmchl@naver.com>
2022-09-20 16:20:47 +0900

    build: create project

---
+++ src/main/java/hello/springmvc12basic/SpringMvc12BasicApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc12basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc12BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc12BasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvc12basic/SpringMvc12BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc12basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc12BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
