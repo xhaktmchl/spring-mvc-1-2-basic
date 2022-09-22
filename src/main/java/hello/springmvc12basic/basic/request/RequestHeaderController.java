2616d65cbee276e4180634d32af458fd26f97db1
xhaktmchl <xhaktmchl@naver.com>
2022-09-22 15:46:29 +0900

    feat: http요청 헤더 조회 컨트롤러 구현

---
+++ src/main/java/hello/springmvc12basic/basic/request/RequestHeaderController.java
@@ -0,0 +1,36 @@
+package hello.springmvc12basic.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+
+    // MultiValueMap 같은 키에 여러 값을 저장 가능
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request, HttpServletResponse response, HttpMethod httpMethod, Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie){
+
+        // 로그 기록
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host); // http 헤더에서 host 속성값 조회
+        log.info("myCookie={}", cookie);
+        return "ok";
+    }
+}
